d52a088ca26be97d5f16141c51b3e36f126ea9ea
8401483508 <prashantbhai22000@student.hamk.fi>
2023-02-20 14:41:30 +0200

    add index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'includes/head.php' ?>
+</head>
+<body id="page-top">
+
+<?php include 'includes/header.php' ?>
+
+<header class="masthead" id="home">
+    <div class="container px-4 px-lg-5 h-100">
+
+    </div>
+</header>
+
+
+
+
+<footer class="bg-light py-5">
+    <?php include 'includes/footer1.php' ?>
+</footer>
+
+<?php include 'includes/bottom.php' ?>
+
+</body>
+</html>
