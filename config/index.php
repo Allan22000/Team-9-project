dc2edfa68110e205765d43d293bacc8a35b62b14
8401483508 <prashantbhai22000@student.hamk.fi>
2023-03-02 20:14:21 +0200

    change index.php

---
+++ config/index.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'includes/head.php' ?>
+</head>
+<body id="page-top">
+
+<?php include 'includes/header.php' ?>
+
+<header class="masthead" id="home">
+    <div class="container px-4 px-lg-5 h-100">
+
+    </div>
+</header>
+
+<section class="page-section" id="about">
+    <div class="container px-4 px-lg-5">
+        <h2 class="text-center mt-0">Founding Real Madrid</h2>
+        <hr class="divider"/>
+        <div class="row gx-4 gx-lg-5">
+            <div class="col-lg-8 col-md-8 text-center">
+                <div class="mt-5" style="text-align:justify;">
+                    <p class="text-muted mb-0">
+                        Real Madrid Club de Fútbol, commonly referred to as Real Madrid or simply as Real is a
+                        professional football club based in Madrid, Spain.
+                    </p>
+                    <p class="text-muted mb-0">
+                        Real Madrid officially adopted its name in 1920, and has since played its home matches in the
+                        Santiago Bernabéu Stadium.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 text-center">
+                <div class="mt-5">
+                    <p class="text-muted mb-0">
+                        <img src="assets/logo.png" style="width: 250px;">
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div id="portfolio">
+    <div class="container-fluid p-3">
+        <div class="row g-0">
+            <div class="col-lg-4 col-sm-6 p-2">
+                <a
+                        class="portfolio-box"
+                        href="assets/img/trophies/fullsize/7.jpeg"
+                        title="The best club of the 20th century Fifa trophy"
+                >
+                    <img
+                            class="img-fluid"
+                            src="assets/img/trophies/fullsize/7.jpeg"
+                            alt="..."
+                    />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50"></div>
+                        <div class="project-name">The best club of the 20th century Fifa trophy</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6 p-2">
+                <a
+                        class="portfolio-box"
+                        href="assets/img/trophies/fullsize/8.jpeg"
+                        title="18 Regional Championship"
+                >
+                    <img
+                            class="img-fluid"
+                            src="assets/img/trophies/fullsize/8.jpeg"
+                            alt="..."
+                    />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">18</div>
+                        <div class="project-name">Regional Championship</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6 p-2">
+                <a
+                        class="portfolio-box"
+                        href="assets/img/trophies/fullsize/1.jpeg"
+                        title="2 Uefa Cups"
+                >
+                    <img
+                            class="img-fluid"
+                            src="assets/img/trophies/thumbnails/1.jpeg"
+                            alt="..."
+                    />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">2</div>
+                        <div class="project-name">Uefa Cups</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6 p-2">
+                <a
+                        class="portfolio-box"
+                        href="assets/img/trophies/fullsize/10.jpeg"
+                        title="5 European Super Cups"
+                >
+                    <img
+                            class="img-fluid"
+                            src="assets/img/trophies/fullsize/10.jpeg"
+                            alt="..."
+                    />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">5</div>
+                        <div class="project-name">European Super Cups</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6 p-2">
+                <a
+                        class="portfolio-box"
+                        href="assets/img/trophies/fullsize/2.jpeg"
+                        title="14 European Cups"
+                >
+                    <img
+                            class="img-fluid"
+                            src="assets/img/trophies/thumbnails/2.jpeg"
+                            alt="..."
+                    />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">14</div>
+                        <div class="project-name">European Cups</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6 p-2">
+                <a
+                        class="portfolio-box"
+                        href="assets/img/trophies/fullsize/3.jpeg"
+                        title="5 Mancomunados Trophies"
+                >
+                    <img
+                            class="img-fluid"
+                            src="assets/img/trophies/thumbnails/3.jpeg"
+                            alt="..."
+                    />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">5</div>
+                        <div class="project-name">Mancomunados Trophies</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6 p-2">
+                <a
+                        class="portfolio-box"
+                        href="assets/img/trophies/fullsize/4.jpeg"
+                        title="35 National Leagues"
+                >
+                    <img
+                            class="img-fluid"
+                            src="assets/img/trophies/thumbnails/4.jpeg"
+                            alt="..."
+                    />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">35</div>
+                        <div class="project-name">National Leagues</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6 p-2">
+                <a
+                        class="portfolio-box"
+                        href="assets/img/trophies/fullsize/5.jpeg"
+                        title="19 Spanish Cups"
+                >
+                    <img
+                            class="img-fluid"
+                            src="assets/img/trophies/thumbnails/5.jpeg"
+                            alt="..."
+                    />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">19</div>
+                        <div class="project-name">Spanish Cups</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6 p-2">
+                <a
+                        class="portfolio-box"
+                        href="assets/img/trophies/fullsize/6.jpeg"
+                        title="12 Spanish Super Cups"
+                >
+                    <img
+                            class="img-fluid"
+                            src="assets/img/trophies/thumbnails/6.jpeg"
+                            alt="..."
+                    />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">12</div>
+                        <div class="project-name">Spanish Super Cups</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6 p-2">
+                <a
+                        class="portfolio-box"
+                        href="assets/img/trophies/fullsize/11.jpeg"
+                        title="7 Fifa Club World Cups"
+                >
+                    <img
+                            class="img-fluid"
+                            src="assets/img/trophies/fullsize/11.jpeg"
+                            alt="..."
+                    />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">7</div>
+                        <div class="project-name">Fifa Club World Cups</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6 p-2">
+                <a class="portfolio-box" href="assets/img/trophies/fullsize/12.jpeg" title="2 Small World Cups" >
+                    <img
+                            class="img-fluid"
+                            src="assets/img/trophies/fullsize/12.jpeg"
+                            alt="..."
+                    />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">2</div>
+                        <div class="project-name">Small World Cups</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6 p-2">
+                <a
+                        class="portfolio-box"
+                        href="assets/img/trophies/fullsize/6.jpeg"
+                        title="1 League Cup"
+                >
+                    <img
+                            class="img-fluid"
+                            src="assets/img/trophies/thumbnails/6.jpeg"
+                            alt="..."
+                    />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">1</div>
+                        <div class="project-name">League Cup</div>
+                    </div>
+
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<section class="page-section" id="contact">
+    <div class="container px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-lg-8 col-xl-6 text-center">
+                <h2 class="mt-0">Let's Get In Touch!</h2>
+                <hr class="divider"/>
+                <p class="text-muted mb-5">Ready to start your next project with us? Send us a messages and we will get
+                    back to you as soon as possible!</p>
+            </div>
+        </div>
+        <div class="row gx-4 gx-lg-5 justify-content-center mb-5">
+            <div class="col-lg-6">
+
+
+                <form id="contactForm" action="base_code/add_new_query.php" method="post">
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="name" type="text" name="firstName" placeholder="Enter your first name..."
+                               required/>
+                        <label for="name">Your First name</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="name" type="text" name="lastName" placeholder="Enter your last name..."
+                               required/>
+                        <label for="name">Your Last name</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="email" type="email" name="email" placeholder="name@example.com" required/>
+                        <label for="email">Your email address</label>
+                    </div>                  
+                    <div class="d-grid">
+                        <button class="btn btn-primary btn-xl" type="submit">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer class="bg-light py-5">
+    <?php include 'includes/footer1.php' ?>
+</footer>
+
+<?php include 'includes/bottom.php' ?>
+
+</body>
+</html>
